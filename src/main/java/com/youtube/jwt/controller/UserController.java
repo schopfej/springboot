2513e7ae5c1136f91d50db9d4d799011ba689e57
schopfej <jonathanschopfer@hotmail.com>
2025-02-27 14:34:48 +0100

    adding two new endpoints: -/forUser -/forAdmin in UserController.java, - 2.) added some initial Data for Admin and User Data

--- src/main/java/com/youtube/jwt/controller/UserController.java
+++ src/main/java/com/youtube/jwt/controller/UserController.java
@@ -5,8 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.youtube.jwt.entity.User;
 import com.youtube.jwt.service.UserService;
+
+import jakarta.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -15,11 +21,28 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+
+    //we want to add this method when app is constructed
+    @PostConstruct
+    public void initRolesAndUser() {
+        userService.initRolesAndUser();
+    }
+
     @PostMapping("/registerNewUser")
 
     public User registerNewUser(@RequestBody User user) {
             return userService.registerNewUser(user);
 
+    }
+
+    //Add two new EndPoints
+    @GetMapping("/forAdmin")
+    public String forAdmin() {
+        return "This URL is only accessible to admin";
+    }
+    @GetMapping("/forUser")
+    public String forUser() {
+        return "This URL is only accessible to the user";
     }
 
 }
--- src/main/java/com/youtube/jwt/service/UserService.java
+++ src/main/java/com/youtube/jwt/service/UserService.java
@@ -1,9 +1,15 @@
 package com.youtube.jwt.service;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Description;
 import org.springframework.stereotype.Service;
 
+import com.youtube.jwt.dao.RoleDao;
 import com.youtube.jwt.dao.UserDao;
+import com.youtube.jwt.entity.Role;
 import com.youtube.jwt.entity.User;
 
 @Service
@@ -11,7 +17,48 @@ public class UserService {
 
     @Autowired
     private UserDao userDao;
+
+    @Autowired
+    private RoleDao roleDao;
+
     public User registerNewUser(User user) {
         return userDao.save(user);
+    }
+
+    // just to make some default roles
+    public void initRolesAndUser() {
+        Role adminRole = new Role();
+        adminRole.setRoleName("Admin");
+        adminRole.setRoleDescription("Description of the Admin");
+        roleDao.save(adminRole);
+
+        Role userRole = new Role();
+        userRole.setRoleName("User");
+        userRole.setRoleDescription("Description of default User");
+        roleDao.save(userRole);
+        
+        User adminUser = new User(); 
+        adminUser.setUserFirstName("admin");
+        adminUser.setUserLastName("admin");
+        adminUser.setUserName("admin1234");
+        adminUser.setUserPassword("admin@password");
+        Set<Role> adminRoles = new HashSet<>();
+        adminRoles.add(adminRole);
+        //that we have an association 
+        adminUser.setRole(adminRoles);
+        userDao.save(adminUser);
+
+        User user = new User(); 
+        user.setUserFirstName("jonathan");
+        user.setUserLastName("schopfer");
+        user.setUserName("schopfej1234");
+        user.setUserPassword("schopfej@password");
+        
+        Set<Role> userRoles = new HashSet<>();
+        userRoles.add(userRole);
+        //again that we have this info in the DB
+        user.setRole(userRoles);
+        userDao.save(user);
+
     }
 }
